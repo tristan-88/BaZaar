ae800617aa701ae6ad8f73baaf7614540d9a78ac
Troy <troyd9216@gmail.com>
2021-04-25 05:00:29 -0500

    created first pass at most of the product and cart routes, group discussion needed

---
+++ app/api/cart_routes.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, jsonify, request
+from flask_login import current_user
+from app.models import db, Product, Store, User, Order, Cart
+
+cart_routes = Blueprint('carts', __name__)
+
+# ---POST--- http://localhost:/5000 api/carts
+# @cart_routes.route('/', methods=["POST"])
+# def create_cart():
+#     #form = CartForm()
+#     if form.validate_on_submit():
+#         cart = Cart(
+#             user_id=current_user.id,
+#             created_at=  # ,
+#             upated_at=  # ,
+#             order_id=  # some query here
+#         )
+#         db.session.add(cart)
+#         db.session.commit()
+#     return jsonify({'cart': cart.to_dict()} if cart else 'Error fetching cart')
+
+#
+
+# ---GET--- http://localhost:5000/api/carts/:id
+
+
+@cart_routes.route('/<int.id>')
+def single_cart(id):
+    cart = Cart.get(id)
+    return cart.to_dict()
+
+# ---PATCH--- http://localhost:5000/api/carts/:id
+
+
+@cart_routes.route('/<int.id>', methods=["PATCH"])
+def edit_cart(id):
+    cart = Cart.get(id)
+    # too tired to think
+    cart = Cart.query.get(id)
+    if cart:
+        content = request.json['content']
+        cart.content = content
+        db.session.commit()
+    return jsonify(cart.to_dict() if cart else 'Failed to update users cart')
+
+# ---DELETE --- http://localhost:5000/api/cart/:id
+
+
+@cart_routes.route('/<int.id>', methods=["Post"])
+def delete_cart(id):
+    remove_cart = Cart.query.filter(Cart.id == id).delete()
+    db.session.commit()
+    return jsonify('Cart was succefully deleted!' if remove_cart else "Could not delete cart")
--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -3,12 +3,14 @@ from flask_login import login_required
 from app.models import db, User
 
 user_routes = Blueprint('users', __name__)
-#for route  testing
+# for route  testing
 
 
-tests = {'Message':'Hello'}
+tests = {'Message': 'Hello'}
 
-#http://localhost:5000/api/users
+# http://localhost:5000/api/users
+
+
 @user_routes.route('/')
 @login_required
 def users():
@@ -16,23 +18,25 @@ def users():
     return {"users": [user.to_dict() for user in users]}
 
 
-#http://localhost:5000/api/users
+# http://localhost:5000/api/users
 @user_routes.route('/', methods=['POST'])
 def create_user():
     return jsonify(tests)
 
 
-#http://localhost:5000/api/users/:id
+# http://localhost:5000/api/users/:id
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
 
-#---DELETE--- http://localhost:5000/api/users/:id ---UNTESTED---
+# ---DELETE--- http://localhost:5000/api/users/:id ---UNTESTED---
+
+
 # @user_routes.route('/<int:id>', methods=['DELETE'])
 # @login_required
 # def delete_user(id):
-#    remove_user = User.query.filter(User.id == id).delete()
+#     remove_user = User.query.filter(User.id == id).delete()
 #     db.session.commit()
-#     return jsonify('User succefully deleted!' if remove_user else 'Could not perform action.')
+#     return jsonify('User succefully deleted!' if remove_user else 'Could not delete user.')
