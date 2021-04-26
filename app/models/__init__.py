aeaa4da23bdd02b793e8b8799777c03305cb73bd
Drew <drewthurmcodes@gmail.com>
2021-04-25 17:54:58 -0700

    moved all models and tables to single file for relationships / associations testing

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,2 @@
 from .db import db
-from .user import User
-from .cart import Cart
-from .order import Order
-from .store import Store
-from .review import Review
-from .product import Product
-from .favorite import Favorite
+from .models import User, Store, Product, Order, Tag, Photo, Review, Cart, cart_product, favorites, product_tag,
