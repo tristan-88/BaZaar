4106c2f027922770d5c209639f9566f8081583a1
Drewthurm21 <drewthurm21@gmail.com>
2021-04-30 12:09:27 -0700

    implemented logic to render proper products on the cart page

--- react-app/src/components/Cart/Cart.js
+++ react-app/src/components/Cart/Cart.js
@@ -1,33 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import './Cart.css'
+import { loadProducts } from '../../store/product'
 import SmallProductTile from '../SmallProductTile/SmallProductTile'
+import './Cart.css'
 
 
 function Cart() {
     const dispatch = useDispatch()
-    //got the state
-    const cartProducts = useSelector(state => state.cart?.products)
-    const  [products, setProducts] = useState([])
+
+    const cartProducts = useSelector(state => state.cart.products)
+    const products = useSelector(state => state.products.products)
+
     //look at when ever something here changes reload this component
     useEffect(() => {
-     setProducts(cartProducts)
-    }, [cartProducts.length, setProducts])
+        dispatch(loadProducts())
+
+    }, [dispatch])
 
 
     return (
         <>
             {products.length &&
-            <div>
-                <div className="cart-card">
-                    <h1>{`CART(${products.length})`}</h1>
-                </div>
                 <div>
-                {products.map(product=>(
-                    <SmallProductTile product={product}/>
-                ))}
+                    <div className="cart-card">
+                        <h1>{`CART(${products.length})`}</h1>
+                    </div>
+                    <div>
+                        {products.map(product => (
+                            <SmallProductTile product={product} />
+                        ))}
+                    </div>
                 </div>
-            </div>
             }
         </>
     )
