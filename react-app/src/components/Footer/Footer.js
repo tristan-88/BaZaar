978a40bd9867a58f5cff306418598668d8b70d24
Tristan San Juan <tristansj88@gmail.com>
2021-06-14 21:02:04 -0700

    change tristan footer info

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -5,34 +5,70 @@ import './Footer.css'
 function Footer () {
 
     return (
-        <>
-            <div className="footer-container">
-            {/* <div className="social-media-container"> */}
-                    <div className='drew-link'>
-                    <h3 style={{color: '#2b2d42'}}>Drew Thurman</h3>
-                    <a href="https://www.linkedin.com/in/troyd41/" target="_blank" style={{textDecoration:'none', fontSize:"2em", color: '#006d77'}}><i className="fab fa-linkedin" style={{fontSize: '3em'}}></i></a>
-                    </div>
-                    <div className='troy-link'>
-                    <h3 style={{color: '#2b2d42'}}>Troy Dawson</h3>
-                         <a href="https://www.linkedin.com/in/troyd41/" target="_blank"  style={{textDecoration:'none', fontSize:"2em",color: '#006d77'}}><i className="fab fa-linkedin" style={{fontSize: '3em'}}></i></a>
-                    </div>
-                    <div className="nat-link">
-                    <h3 style={{color: '#2b2d42'}}>Nathaniel Ortega</h3>
-                    <a href="https://www.linkedin.com/in/nathaniel-ortega-256673206/" target="_blank" style={{textDecoration:'none', fontSize:"2em",color: '#006d77'}}><i className="fab fa-linkedin" style={{fontSize: '3em'}}></i></a>
-                    </div>
+			<>
+				<div className="footer-container">
+					{/* <div className="social-media-container"> */}
+					<div className="drew-link">
+						<h3 style={{ color: "#2b2d42" }}>Drew Thurman</h3>
+						<a
+							href="https://www.linkedin.com/in/troyd41/"
+							target="_blank"
+							style={{
+								textDecoration: "none",
+								fontSize: "2em",
+								color: "#006d77",
+							}}
+						>
+							<i className="fab fa-linkedin" style={{ fontSize: "3em" }}></i>
+						</a>
+					</div>
+					<div className="troy-link">
+						<h3 style={{ color: "#2b2d42" }}>Troy Dawson</h3>
+						<a
+							href="https://www.linkedin.com/in/troyd41/"
+							target="_blank"
+							style={{
+								textDecoration: "none",
+								fontSize: "2em",
+								color: "#006d77",
+							}}
+						>
+							<i className="fab fa-linkedin" style={{ fontSize: "3em" }}></i>
+						</a>
+					</div>
+					<div className="nat-link">
+						<h3 style={{ color: "#2b2d42" }}>Nathaniel Ortega</h3>
+						<a
+							href="https://www.linkedin.com/in/nathaniel-ortega-256673206/"
+							target="_blank"
+							style={{
+								textDecoration: "none",
+								fontSize: "2em",
+								color: "#006d77",
+							}}
+						>
+							<i className="fab fa-linkedin" style={{ fontSize: "3em" }}></i>
+						</a>
+					</div>
 
-                    <div className="tristan-git">
-                    <h3 style={{color: '#2b2d42'}}>Tristan San-Juan</h3>
-                    <a href="https://github.com/tristan-88" target="_blank" style={{textDecoration:'none', fontSize:"2em", color: '#006d77'}}><i className="fab fa-github" style={{fontSize: '3em'}}></i></a>
-                    </div>
-                </div>
-            {/* </div> */}
-
-
-
-
-        </>
-    )
+					<div className="tristan-git">
+						<h3 style={{ color: "#2b2d42" }}>Tristan San-Juan</h3>
+						<a
+							href="https://www.linkedin.com/in/tristan-san-juan-75337920b/"
+							target="_blank"
+							style={{
+								textDecoration: "none",
+								fontSize: "2em",
+								color: "#006d77",
+							}}
+						>
+							<i className="fab fa-linkedin" style={{ fontSize: "3em" }}></i>
+						</a>
+					</div>
+				</div>
+				{/* </div> */}
+			</>
+		);
 }
 
 export default Footer
